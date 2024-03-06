1fdabc27caf5f23d66d39cb8354e70df2813dcd2
Rufet Huseynov <huseyinovrufet49@gmail.com>
2024-03-06 14:29:12 +0400

    First commit

--- src/main/java/com/backend101/marketmanagementsystem/service/WarehouseService.java
+++ src/main/java/com/backend101/marketmanagementsystem/service/WarehouseService.java
@@ -2,11 +2,31 @@ package com.backend101.marketmanagementsystem.service;
 
 import com.backend101.marketmanagementsystem.dto.request.WarehouseByFkEmployeeIdRequestDTO;
 import com.backend101.marketmanagementsystem.dto.response.WarehouseResponseDTO;
+import com.backend101.marketmanagementsystem.mapper.WarehouseMapper;
+import com.backend101.marketmanagementsystem.repository.WarehouseRepository;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-public interface WarehouseService {
+@Service
+@RequiredArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+public class WarehouseService {
 
-    List<WarehouseResponseDTO> findAllProduct(); //Filtersiz butun mehsullar gelmelidir
-    List<WarehouseResponseDTO> findAllProductByFkEmployeeId(WarehouseByFkEmployeeIdRequestDTO dto); //EmployeeId-e gore
+    WarehouseRepository warehouseRepository;
+    WarehouseMapper warehouseMapper;
+
+    public List<WarehouseResponseDTO> findAllProduct() {
+        return warehouseRepository.findAll().stream()
+                .map(warehouseMapper::toWarehouseResponseDto).toList();
+    }
+
+
+    public List<WarehouseResponseDTO> findAllProductByFkEmployeeId(WarehouseByFkEmployeeIdRequestDTO dto) {
+        return warehouseRepository.findAllByFkEmployeeId(dto.getFkEmployeeId()).stream()
+                .map(warehouseMapper::toWarehouseResponseDto).toList();
+    }
 }
