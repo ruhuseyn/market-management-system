5e85ce9d28d6e3dd797221e43a5f70a6e702b7e0
Rufet Huseynov <huseyinovrufet49@gmail.com>
2024-03-08 12:41:34 +0400

    First commit

--- src/main/java/com/backend101/marketmanagementsystem/mapper/SaleMapper.java
+++ src/main/java/com/backend101/marketmanagementsystem/mapper/SaleMapper.java
@@ -1,6 +1,8 @@
 package com.backend101.marketmanagementsystem.mapper;
 
+import com.backend101.marketmanagementsystem.dto.dto.SaleReadDTO;
 import com.backend101.marketmanagementsystem.dto.request.SaleRequestDTO;
+import com.backend101.marketmanagementsystem.dto.response.SaleReadResponseDTO;
 import com.backend101.marketmanagementsystem.entity.SaleEntity;
 import org.mapstruct.Mapper;
 
@@ -8,4 +10,6 @@ import org.mapstruct.Mapper;
 public interface SaleMapper {
 
     SaleEntity toEntity(SaleRequestDTO dto);
+
+    SaleReadDTO toSaleReadDTO(SaleEntity entity);
 }
