5378256413d75a4eae1c2992c37ddd0f85bfe3fe
Rufet Huseynov <huseyinovrufet49@gmail.com>
2024-03-08 12:41:53 +0400

    First commit

---
+++ src/main/java/com/backend101/marketmanagementsystem/exception/notfound/WarehouseNotFoundException.java
@@ -0,0 +1,7 @@
+package com.backend101.marketmanagementsystem.exception.notfound;
+
+public class WarehouseNotFoundException extends RuntimeException {
+    public WarehouseNotFoundException(String message) {
+        super(message);
+    }
+}
